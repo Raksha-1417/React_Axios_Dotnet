40d1aca7bd4723009f3502f986746015ad2623f8
Raksha-1417 <raksha.rk14@gmail.com>
2022-10-24 18:26:24 +0530

    Add project files.

---
+++ Models/Detail.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace React_Axios_Dotnet.Models
+{
+    public partial class Detail
+    {
+        [Key]
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Image { get; set; }
+        public string? Description { get; set; }
+    }
+}
---
+++ Models/ReactAxiosAPIContext.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace React_Axios_Dotnet.Models
+{
+    public partial class ReactAxiosAPIContext : DbContext
+    {
+        public ReactAxiosAPIContext()
+        {
+        }
+
+        public ReactAxiosAPIContext(DbContextOptions<ReactAxiosAPIContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Detail> Details { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                //optionsBuilder.UseSqlServer("Server=ELW5243\\SQLEXPRESS;Database=ReactAxiosAPI;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Detail>(entity =>
+            {
+               // entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.Description)
+                    .HasMaxLength(300)
+                    .IsUnicode(false)
+                    .HasColumnName("description");
+
+                entity.Property(e => e.Image)
+                    .HasMaxLength(300)
+                    .IsUnicode(false)
+                    .HasColumnName("image");
+
+                entity.Property(e => e.Title)
+                    .HasMaxLength(300)
+                    .IsUnicode(false)
+                    .HasColumnName("title");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
